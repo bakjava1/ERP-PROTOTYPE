2f6ce43c3b0bb695169772473daab6cb4bffe81d
Philipp Klein <e1326251@student.tuwien.ac.at>
2017-12-13 20:08:05 +0100

    Task #21787 edit order: added alertbuilder and gitignore (.iml)

---
+++ client/src/main/java/at/ac/tuwien/sepm/assignment/group02/client/gui/AlertBuilder.java
@@ -0,0 +1,70 @@
+package at.ac.tuwien.sepm.assignment.group02.client.gui;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextInputDialog;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+public class AlertBuilder {
+    public void showInformationAlert(String title, String header, String content){
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(header);
+        alert.setContentText(content);
+        alert.showAndWait();
+    }
+
+    public void showErrorAlert(String title, String header, String content){
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setHeaderText(header);
+        alert.setContentText(content);
+        alert.showAndWait();
+    }
+
+    public boolean showConfirmationAlert(String title, String header, String content){
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(header);
+        alert.setContentText(content);
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == ButtonType.OK) {
+            return true;
+        } else {
+            alert.close();
+            return false;
+        }
+    }
+
+    public String showCustomConfirmationAlert(String title, String header, String content, String[] buttonText){
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(header);
+        alert.setContentText(content);
+
+        ArrayList<ButtonType> buttonTypes = new ArrayList<>();
+        for(int i = 0; i<buttonText.length; i++){
+            buttonTypes.add(new ButtonType(buttonText[i]));
+        }
+        buttonTypes.add(new ButtonType("Cancel", ButtonBar.ButtonData.CANCEL_CLOSE));
+
+        alert.getButtonTypes().setAll(buttonTypes);
+        Optional<ButtonType> result = alert.showAndWait();
+        return result.get().getText();
+    }
+
+    public String showTextInputDialog(String title, String header, String content, String defaultValue){
+        TextInputDialog dialog = new TextInputDialog(defaultValue);
+        dialog.setTitle(title);
+        dialog.setHeaderText(header);
+
+        Optional<String> result = dialog.showAndWait();
+        if (result.isPresent()){
+            return result.get();
+        }
+        return null;
+    }
+}
