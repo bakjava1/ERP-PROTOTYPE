c39797801c0d5331c3e359359d6e71946c69155d
dogankaraarslan <e1529060@student.tuwien.ac.at>
2017-12-17 03:16:34 +0100

    Task #22146 Rechnungen GUI erstellt

--- client/src/main/java/at/ac/tuwien/sepm/assignment/group02/client/gui/OfficeFXML.java
+++ client/src/main/java/at/ac/tuwien/sepm/assignment/group02/client/gui/OfficeFXML.java
@@ -101,8 +101,27 @@ public class OfficeFXML {
     @FXML
     private TableColumn col_grossSum;
 
+
+    @FXML
+    private TableColumn col_billID;
+
+    @FXML
+    private TableColumn col_billCostumerName;
+
+    @FXML
+    private TableColumn col_billTaskAmount;
+
+    @FXML
+    private TableColumn col_billAmount;
+
+    @FXML
+    private TableColumn col_billGrossSum;
+
     @FXML
     TableView<Order> table_openOrder;
+
+    @FXML
+    TableView<Order> table_bill;
 
     private OrderService orderService;
     private TimberService timberService;
@@ -127,6 +146,14 @@ public class OfficeFXML {
         col_grossSum.setCellValueFactory(new PropertyValueFactory("grossAmount"));
 
 
+
+        table_bill.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+
+        col_billID.setCellValueFactory(new PropertyValueFactory("ID"));
+        col_billCostumerName.setCellValueFactory(new PropertyValueFactory("customerName"));
+        col_billTaskAmount.setCellValueFactory(new PropertyValueFactory("taskAmount"));
+        col_billAmount.setCellValueFactory(new PropertyValueFactory("quantity"));
+        col_billGrossSum.setCellValueFactory(new PropertyValueFactory("grossAmount"));
 
 
 
@@ -155,6 +182,7 @@ public class OfficeFXML {
 
         initTimberTab();
         updateTable();
+        updateBillTable();
     }
 
     @FXML
@@ -257,6 +285,37 @@ public class OfficeFXML {
         }
 
     }
+
+
+    private void updateBillTable() {
+
+        List<Order> allClosed = null;
+        ObservableList<Order> closedOrderForTable;
+
+        try {
+            allClosed = orderService.getAllClosed();
+        } catch (ServiceLayerException e) {
+            LOG.warn(e.getMessage());
+        }
+
+        if (allClosed != null) {
+
+            closedOrderForTable = FXCollections.observableArrayList();
+
+            for (Order bill: allClosed) {
+                closedOrderForTable.add(bill);
+            }
+
+            table_bill.setItems(closedOrderForTable);
+            table_bill.refresh();
+        } else {
+            table_bill.refresh();
+        }
+
+    }
+
+
+
 
     @FXML
     public void addTimber(ActionEvent actionEvent){
