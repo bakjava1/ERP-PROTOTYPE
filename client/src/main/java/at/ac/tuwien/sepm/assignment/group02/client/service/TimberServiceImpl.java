d71d8c814d4ffb2bb44537366541ec08945b60b0
Philipp Klein <e1326251@student.tuwien.ac.at>
2017-12-20 15:07:05 +0100

    Bug #22372: add Validator in add timber

--- client/src/main/java/at/ac/tuwien/sepm/assignment/group02/client/service/TimberServiceImpl.java
+++ client/src/main/java/at/ac/tuwien/sepm/assignment/group02/client/service/TimberServiceImpl.java
@@ -6,6 +6,7 @@ import at.ac.tuwien.sepm.assignment.group02.client.exceptions.ServiceLayerExcept
 import at.ac.tuwien.sepm.assignment.group02.client.rest.TimberController;
 import at.ac.tuwien.sepm.assignment.group02.client.converter.TimberConverter;
 import at.ac.tuwien.sepm.assignment.group02.client.entity.Timber;
+import at.ac.tuwien.sepm.assignment.group02.client.validation.ValidateInput;
 import at.ac.tuwien.sepm.assignment.group02.rest.restDTO.TimberDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,22 +22,22 @@ public class TimberServiceImpl implements TimberService{
 
     private static TimberController timberController;
     private static TimberConverter timberConverter;
+    private static ValidateInput<Timber> timberValidator;
 
     @Autowired
-    public TimberServiceImpl(TimberController timberController, TimberConverter timberConverter){
+    public TimberServiceImpl(TimberController timberController, TimberConverter timberConverter, ValidateInput<Timber> timberValidator){
         TimberServiceImpl.timberController = timberController;
         TimberServiceImpl.timberConverter = timberConverter;
+        TimberServiceImpl.timberValidator = timberValidator;
     }
 
     @Override
     public void addTimber(Timber timber) throws ServiceLayerException, InvalidInputException{
 
         try {
-            if(timber.getBox_id()>timberController.getNumberOfBoxes() || timber.getBox_id()<0)
-                throw new InvalidInputException("error finding box with given id");
-
-            if(timber.getAmount()<0)
-                throw new InvalidInputException("error cannot add negative amount of timber");
+            if(timber.getBox_id()>timberController.getNumberOfBoxes())
+                throw new InvalidInputException("Error in Timber Box: Box ID is bigger than number of boxes");
+            timberValidator.isValid(timber);
         } catch (PersistenceLayerException e) {
             LOG.warn(e.getMessage());
             throw new ServiceLayerException(e.getMessage());
