9b91313ba7026d142506320b47b4723b92d7b7f1
Lucia Lechner <e0701149@student.tuwien.ac.at>
2018-01-21 13:16:19 +0100

    Task #23175: fixed lumber search according to validator change

--- client/src/main/java/at/ac/tuwien/sepm/assignment/group02/client/gui/LeadWorkerFXML.java
+++ client/src/main/java/at/ac/tuwien/sepm/assignment/group02/client/gui/LeadWorkerFXML.java
@@ -184,9 +184,9 @@ public class LeadWorkerFXML {
         col_quantity.setCellValueFactory(new PropertyValueFactory<>("quantity"));
         col_reserved_quantity.setCellValueFactory(new PropertyValueFactory<>("reserved_quantity"));
 
-        cb_finishing.setItems(FXCollections.observableArrayList("keine Angabe", "roh", "gehobelt", "besäumt", "prismiert", "trocken","lutro","frisch", "imprägniert"));
-        cb_wood_type.setItems(FXCollections.observableArrayList("keine Angabe", "Fi", "Ta", "Lae"));
-        cb_quality.setItems(FXCollections.observableArrayList("keine Angabe", "O","I","II","III","IV","V", "O/III", "III/IV", "III/V"));
+        cb_finishing.setItems(FXCollections.observableArrayList("alle Veredelungen", "roh", "gehobelt", "besäumt", "prismiert", "trocken","lutro","frisch", "imprägniert"));
+        cb_wood_type.setItems(FXCollections.observableArrayList("alle Holzarten", "Fi", "Ta", "Lae"));
+        cb_quality.setItems(FXCollections.observableArrayList("alle Qualitäten", "O","I","II","III","IV","V", "O/III", "III/IV", "III/V"));
 
         cb_finishing.getSelectionModel().selectFirst();
         cb_wood_type.getSelectionModel().selectFirst();
@@ -241,7 +241,7 @@ public class LeadWorkerFXML {
                 .getResource("reset_search.png");
         if(imgpath!=null) {
             ImageView imageView = new ImageView(new Image(imgpath.toString()));
-            imageView.setFitWidth(30.00);
+            imageView.setFitWidth(25.00);
             imageView.setPreserveRatio(true);
             btn_resetSearch.setGraphic(imageView);
         } else {
