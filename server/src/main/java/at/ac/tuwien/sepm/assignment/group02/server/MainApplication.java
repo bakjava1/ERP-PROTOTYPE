c5c8de026a36aced31439d2b1afdaa8b7563d91b
DoganKaraarslan <32490090+DoganKaraarslan@users.noreply.github.com>
2023-06-11 03:07:43 +0200

    test

--- server/src/main/java/at/ac/tuwien/sepm/assignment/group02/server/MainApplication.java
+++ server/src/main/java/at/ac/tuwien/sepm/assignment/group02/server/MainApplication.java
@@ -4,12 +4,14 @@ import at.ac.tuwien.sepm.assignment.group02.server.util.DBUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import javax.annotation.PreDestroy;
 import java.lang.invoke.MethodHandles;
+
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"at.ac.tuwien.sepm.assignment.group02.server"})
